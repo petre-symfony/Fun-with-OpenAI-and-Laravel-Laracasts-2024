724f915157f620447a2713ed2bf27df8533d5408
petrero <petrero@gmail.com>
2024-02-07 12:24:26 +0200

    2.1 All right, welcome back, everybody. So what you see here is where we left off at the conclusion of our introductory Hello World example.  But now I want to talk about how we could go about continuing the conversation. And here's what I mean. We started it by saying, well, compose a poem that explains the concept of recursion and programming, right? And it gives us a response.  But what if we then want to reply back and say, okay, very good, but now make it much more silly or funny? How do we go about doing that? And actually, as it turns out, the way we go about doing that is by assuming that OpenAI has no short-term memory.  And I'll show you what I mean. Okay, so the first thing I want to do is grab this array of messages, and I'm going to extract it into its own variable. Okay, so we can do that right up here at the top. All right, so now right down here, we fetch our first response, and that happens to be the poem.  So here's what I'm going to do. I'm going to push to that messages array. And this will be, well, effectively, this. So I can grab that and paste it in. But now the content is going to be equal to the poem itself. And the role is not going to be us, the user. It's going to be OpenAI. And we usually call this assistant. Okay, so now at this point, once we get to this line, our messages array consists of our system message where we set the world. It has our initial message, and then it also has the poem response. Okay, so for example, if I were to die and dump this in the browser, let's have a look. Here we go. So now we have our messages array that contains the system message, our conversation starter, and then the poem response from our assistant or OpenAI.  Okay, so now here's what I mean when I said we should assume that OpenAI has no short-term memory. If I want to continue the conversation, what I really need to do is send this entire array back to OpenAI. So it's almost like every time I want to continue the chat, I have to remind OpenAI of everything that was said leading up to that very moment. All right, so that's why we're now storing it within an array.

--- routes/web.php
+++ routes/web.php
@@ -15,20 +15,28 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+	$messages = [
+		[
+			"role" => "system",
+			"content" => "You are a poetic assistant, skilled in explaining complex concepts with creative flair"
+		],
+		[
+			"role" => "user",
+			"content" => "Compose a poem that explains the concept of trcursion in programming"
+		]
+	];
 	$poem = Http::withToken(config('services.openai.secret'))->post('https://api.openai.com/v1/chat/completions', [
 		"model" => "gpt-3.5-turbo",
-		"messages" => [
-			[
-				"role" => "system",
-				"content" => "You are a poetic assistant, skilled in explaining complex concepts with creative flair"
-			],
-			[
-				"role" => "user",
-				"content" => "Compose a poem that explains the concept of trcursion in programming"
-			]
-		]
+		"messages" => $messages
 	])->json('choices.0.message.content');
 
+	$messages[] = [
+		"role" => "assistant",
+		"content" => $poem
+	];
+
+	dd($messages);
+	
 	return view('welcome', [
 		'poem' => $poem
 	]);
