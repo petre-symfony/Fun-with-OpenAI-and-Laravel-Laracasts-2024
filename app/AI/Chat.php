32968d11e98984ac933f3ed52cb72882232203b4
petrero <petrero@gmail.com>
2024-02-07 12:48:33 +0200

    2.4 Or remember, one fun thing about developing little APIs like this is, you can be as creative as you want. So if you want to keep it kind of lifelike, you could create a method called say. All right, now I'm going to say this to OpenAI, if you want.  But yeah, for now, I'm just going to keep it with a little more technical. I will call it sends. And how would that work? I want to send a new message, tell me a bedtime story, or something, you know, kind of dark like that.  So could we make that work? Well, hmm, it sounds like I need a new method here called sends. And this will give us a message. And as we learned, let's open up a split here. As we learned, what we really need to do is, excuse me, is we need to append to our root messages array. So probably what we would do at this point is say, well, this messages push a new array where the role is user, and the content is whatever that message happens to be.  All right, but next, this is just adding a new message. And maybe that can be useful. But the method is called sends. So we actually need to perform the API request here, don't we? So what I will do, once again, is return to route slash web.  And yeah, we're going to grab this chunk here. And I'm going to paste it in. All right, let's import that. Of course, you could grab the underlying. What would it be? Is it a factory? Is it a pending request? I can't remember. But you could use dependency injection if you want. All right, so HTTP with token, we're going to make a POST request to the completions API.  Again, all of this could be configurable. So if you want to use a unique language model for certain messages,then you could make that configurable as well and define it as a property. OK, the messages will reference that property.  We get the response. Now we've learned the response should also be appended to the messages array. So once again, I could duplicate this and then say role assistant response. All right. And then finally, I have to decide when I call that send method, what do I want returned? Is void returned? Is the response message itself returned? Is the messages array? I think for now, I'd prefer that the response message is returned.  So return the response. All right. So I will let you take a look at what we have so far.  And yeah, we're just sort of winging it. We can tweak it if we need to. But I think this will end up being quite a bit more flexible than what we had before.

--- app/AI/Chat.php
+++ app/AI/Chat.php
@@ -2,11 +2,33 @@
 
 namespace App\AI;
 
+use Illuminate\Support\Facades\Http;
+
 class Chat {
 	protected array $messages = [];
 
 	public function messages() {
 		return $this->messages;
 	}
+
+	public function send(string $message): string {
+		$this->messages[] = [
+			'role' => 'user',
+			'content' => $message
+		];
+
+		$response = Http::withToken(config('services.openai.secret'))
+			->post('https://api.openai.com/v1/chat/completions', [
+				"model" => "gpt-3.5-turbo",
+				"messages" => $this->messages
+			])->json('choices.0.message.content');
+
+		$this->messages[] = [
+			'role' => 'assistant',
+			'content' => $response
+		];
+
+		return $response;
+	}
 }
-//$chat->messages()
+//$chat->send('Tell me a bad time story')
