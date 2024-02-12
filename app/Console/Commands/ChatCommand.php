0f69a60740d591cf68abc8ad9ecc757812ec5575
petrero <petrero@gmail.com>
2024-02-12 16:02:40 +0200

    3.5 php artisan make:command ChatCommand  Okay, so now we can move on to the example for this episode, and here's what I was thinking. It might be fun if we created a little command line utility for, once again, chatting or interacting with OpenAI, but now we can do it directly from the command line, so we'd have the ability to ask a question, follow up, ask for clarification, and I can just keep that conversation going, again, directly from the command line. Okay, and also on that note, it would be cool if we can also pull in the Laravel prompts package just to make the whole interface a little bit more attractive. All right, let's see what we can come up with in 10 minutes or so.  So if I come to the command line, yeah, I'm thinking something like this, php artisan chat. So let's make that command.

---
+++ app/Console/Commands/ChatCommand.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class ChatCommand extends Command {
+	/**
+	 * The name and signature of the console command.
+	 *
+	 * @var string
+	 */
+	protected $signature = 'app:chat';
+
+	/**
+	 * The console command description.
+	 *
+	 * @var string
+	 */
+	protected $description = 'Command description';
+
+	/**
+	 * Execute the console command.
+	 */
+	public function handle() {
+		//
+	}
+}
