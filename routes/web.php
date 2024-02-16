2f434bb477922d7cf03de715c31e83a3d473ac25
petrero <petrero@gmail.com>
2024-02-16 19:17:28 +0200

    7.2 So now I'm going to go into my routes file and take a second pass here. So if it turns out all of this is effectively a validation rule, let's start by inlining it here along with the other validation rules. And we can use a closure here. The first argument will be the attribute name. So this would be body. The next one is the value of the text area. And then the final parameter will be a closure that you can call to trigger the validation exception. Okay. So here's what I'm going to do. I will grab all of this junk. Yeah, all of that.  That represents our rule. And I will paste it in and reformat.

--- routes/web.php
+++ routes/web.php
@@ -21,34 +21,39 @@ Route::get('/detect_spam', function (){
 
 Route::post('/replies', function() {
 	$attributes = request()->validate([
-		'body' => ['required', 'string']
+		'body' => [
+			'required',
+			'string',
+			function ($attribute, $value, $fail) {
+				$response = OpenAI::chat()->create([
+					'model' => 'gpt-3.5-turbo-1106',
+					'messages' => [
+						['role' => 'system', 'content' => 'You are a forum moderator who always responds using json'],
+						[
+							'role' => 'user',
+							'content' => <<<EOT
+								Please inspect the following text and determine if it is spam.
+								
+								{$attributes['body']}
+								
+								Expected response example:
+								{"is_spam": true|false}
+							EOT
+						],
+					],
+					'response_format' => ['type' => 'json_object']
+				])->choices[0]->message->content;
+
+				$response = json_decode($response);
+
+				if ($response->is_spam) {
+					throw ValidationException::withMessages([
+						'body' => 'Spam was detected'
+					]);
+				};
+			}
+		]
 	]);
-
-	$response = OpenAI::chat()->create([
-		'model' => 'gpt-3.5-turbo-1106',
-		'messages' => [
-			['role' => 'system', 'content' => 'You are a forum moderator who always responds using json'],
-			['role' => 'user',
-				'content' => <<<EOT
-					Please inspect the following text and determine if it is spam.
-					
-					{$attributes['body']}
-					
-					Expected response example:
-					{"is_spam": true|false}
-				EOT
-			],
-		],
-		'response_format' => ['type' => 'json_object']
-	])->choices[0]->message->content;
-
-	$response = json_decode($response);
-
-	if ($response->is_spam) {
-		throw ValidationException::withMessages([
-			'body' => 'Spam was detected'
-		]);
-	};
 
 	return 'Redirect wherever is needed. Post was valid';
 });
