ed2c798fb585c9b90bd8e7b0bacdf6e9380bc005
petrero <petrero@gmail.com>
2024-02-16 19:35:36 +0200

    7.4 php artisan make:rule SpamFree  But why don't we take it one step further and extract a validation rule class? From the command line, I can say phpArtisan make rule.  And what is the rule here? The rule is it's not spam. So why don't we call it spam free?  All right. Cool.

---
+++ app/Rules/SpamFree.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Rules;
+
+use Closure;
+use Illuminate\Contracts\Validation\ValidationRule;
+
+class SpamFree implements ValidationRule {
+	/**
+	 * Run the validation rule.
+	 *
+	 * @param \Closure(string): \Illuminate\Translation\PotentiallyTranslatedString $fail
+	 */
+	public function validate(string $attribute, mixed $value, Closure $fail): void {
+		//
+	}
+}
