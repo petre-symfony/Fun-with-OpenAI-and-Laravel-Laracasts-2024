be21aa44f538cbee3cf8fbba86f7f9672688af61
petrero <petrero@gmail.com>
2024-03-06 19:56:20 +0200

    10.3 Okay, so now let's move on to creating our client.  Now, if I scroll the page here, you're going to see countless calls to OpenAI's API. So here's another one. Here's another one. Here's another one. Here's another one. Here's another one. It just keeps happening. And generally, you know, depending upon the size of the project, this may or may not be a problem. On that note, I don't know. Something to keep in mind is, from all the years I've been doing this, there's definitely a tendency to want to be an architect. You know, like, oh, we can't just have this within a single file.  We need design. We need to adhere to these solid principles. So, and often what ends up happening is they just take code here, and then they paste it into this new file, and then they pat themselves on the back for creating better design or allowing for better design.  And I know maybe sometimes that happens. It does have its benefits. It allows you to isolate things better. It allows you to mock a dependency so that you can test faster. But also sometimes it doesn't really do much. You just moved it from here to there, you know. And you've got to remember that the size of the people working on the project makes a big difference. If it's just you, the rules are different. I'm sorry, it's just true. The rules are different than if you're a member of Facebook and you're working with literally hundreds of different people. So yeah, all I have to say, just keep this in mind. What I'm about to show you is just a possibility.  And sometimes you may decide, oh, that's what I should do because I know what's about to happen or I know certain flexibility is needed. But then other times, it just doesn't matter. And that's something I've tried to reiterate over the years. All right, back to work.  So what we're going to do is I'll come back to our AI class, and I'll just put it right here. Let's create a new class called Client. Think of this as the client that interacts with OpenAI. So what I could do is find all of the relevant methods that we have here that are specific to an OpenAI call.  So for example, let's start at the very top, OpenAI::assistants()->retrieve($assistantId) , Retrieving an Assistant. So let's open up our client and maybe add a method called Retrieve Assistant. All right, next, OpenAI::assistants()->create(array_merge_recursive([]) this one creates an assistant. So let's do that one. All right, what else? This one uploads a file, OpenAI::files()->upload([]) So let's add a method called Upload File. All right, what else? We have one for creating a thread, OpenAI::threads()->create($parameters). So Create Thread.  And then right down here, we have another one for creating a message, OpenAI::threads()->messages()->create($this->threadId, []) So let's duplicate that as well. And yeah, this may seem redundant, and sometimes it is. But also sometimes it gives you some extra flexibility.  Here Fetch All Messages, OpenAI::threads()->messages(). So this is effectively a getter. We could say Get Messages or just Messages if you wish. And then finally, we have an API call to Start a Run, , OpenAI::threads()->runs()->create($this->threadId, []) So why don't we just call that one Run.  And then lastly, we have one that checks a run's status.   $run = OpenAI::threads()->runs()->retrieve( 	threadId: $this->threadId, 	runId: $run->id   );
      return $run->status ! == 'completed';
      So and yeah, we might want to tweak these names, but this is the 30-second pass.

---
+++ app/AI/Client.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\AI;
+
+class Client {
+	public function retrieveAssistant() {
+		
+	}
+
+	public function createAssistant() {
+		
+	}
+
+	public function uploadFile() {
+		
+	}
+
+	public function createThread() {
+		
+	}
+
+	public function createMessage() {
+		
+	}
+
+	public function messages() {
+		
+	}
+
+	public function run() {
+		
+	}
+
+	public function runStatus() {
+		
+	}
+}
